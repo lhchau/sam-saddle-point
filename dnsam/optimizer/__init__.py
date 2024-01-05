a2f09174b01a8fbef0abda084e48a0d522267b34
lhchau <lhchau20@apcs.fitus.edu.vn>
2024-01-05 19:26:53 +0700

    feat: add dnsam restart

--- dnsam/optimizer/__init__.py
+++ dnsam/optimizer/__init__.py
@@ -1,3 +1,4 @@
 from .sam import SAM
 from .sam_faster import SAM_Faster
 from .dnsam import DNSAM
+from .dnsam_restart import RDNSAM
--- dnsam/utils/get_optimizer.py
+++ dnsam/utils/get_optimizer.py
@@ -1,4 +1,4 @@
-from ..optimizer import SAM, SAM_Faster, DNSAM
+from ..optimizer import SAM, SAM_Faster, DNSAM, RDNSAM
 
 def get_optimizer(net, base_optimizer, cfg):
     if cfg['model']['name'] == 'sam':
@@ -34,6 +34,19 @@ def get_optimizer(net, base_optimizer, cfg):
             adaptive=cfg['model']['adaptive'],
             nesterov=cfg['model']['nesterov'],
             dnsam_theta=cfg['model']['dnsam_theta']
+        )
+    elif cfg['model']['name'] == 'rdnsam':
+        return RDNSAM(
+            net.parameters(), 
+            base_optimizer, 
+            lr=cfg['model']['lr'], 
+            momentum=cfg['model']['momentum'], 
+            weight_decay=cfg['model']['weight_decay'],
+            rho=cfg['model']['rho'], 
+            adaptive=cfg['model']['adaptive'],
+            nesterov=cfg['model']['nesterov'],
+            dnsam_theta=cfg['model']['dnsam_theta'],
+            restart_step=cfg['model']['restart_step']
         )
     else:
         raise ValueError("Invalid optimizer!!!")
