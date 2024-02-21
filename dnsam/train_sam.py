eb01eeb0e2ddee6e61b5daa2f4becd5ea86b1969
lhchau <lhchau20@apcs.fitus.edu.vn>
2024-02-21 16:15:59 +0700

    config small rho

--- dnsam/train_sam.py
+++ dnsam/train_sam.py
@@ -115,16 +115,16 @@ def train(epoch):
         optimizer.zero_grad()
         
         # get cosine similarity
-        # similarity = np.mean([cosine_similarity(grad1, grad2) for grad1, grad2 in zip(first_grads, second_grads)])
+        similarity = np.mean([cosine_similarity(grad1, grad2) for grad1, grad2 in zip(first_grads, second_grads)])
         # grad_norm, hessian_norm, scale, weight_norm1, weight_norm2 = optimizer.get_norm()
-        # wandb.log({
-        #     'similarity': similarity,
+        wandb.log({
+            'similarity': similarity,
         #     'grad_norm': grad_norm,
         #     'hessian_norm': hessian_norm,
         #     'scale': scale,
         #     'weight_norm1': weight_norm1,
         #     'weight_norm1': weight_norm2
-        # })
+        })
 
         
         train_loss += first_loss.item()
